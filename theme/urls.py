17031a7df2150138937a3488122d9f413dde3050
Niraj chaudhary <124789047+niraj907@users.noreply.github.com>
2025-06-17 21:46:56 -0700

    code has been update document and urls and views and contact and login

--- theme/urls.py
+++ theme/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index,contact, login , signup
-
+from .views import index,contact,document,view ,download_view ,  login , signup
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', contact, name='contact'), 
+    path('document/', document, name='document'), 
+    path('view/', view, name='view'),  
+    path('download/', download_view, name='download'),
     path('login/', login, name='login'), 
     path('signup/', signup, name='signup'), 
     path('', index, name='index'),
--- theme/views.py
+++ theme/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+
 def index(request):
     return render(request, 'hero.html')
+def document(request):
+    return render(request, 'document.html')
+def view(request):
+    return render(request, 'view.html')
 def contact(request):
     return render(request, 'contact.html')
 def login(request):
     return render(request, 'login.html')
 def signup(request):
     return render(request, 'signup.html')
+
+
+def download_view(request):
+    html_content = render_to_string('view.html')
+    response = HttpResponse(html_content, content_type='application/octet-stream')
+    response['Content-Disposition'] = 'attachment; filename="report.html"'
+    return response
