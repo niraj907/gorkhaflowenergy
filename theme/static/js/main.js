6850a86aee13bf289c78308a542342d638c54ce0
Niraj chaudhary <124789047+niraj907@users.noreply.github.com>
2025-06-20 02:23:40 -0700

    update in nav bar and hero

--- theme/static/js/main.js
+++ theme/static/js/main.js
@@ -1,15 +1,8 @@
-const menuBtn = document.getElementById('menuToggle')
-const mobileMenu = document.getElementById('mobileMenu')
+    const menuButtons = document.querySelectorAll('.menu-btn');
+    const mobileMenus = document.querySelectorAll('.mobile-menu');
 
-menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden')
-})
-
-const menuButtons = document.querySelectorAll('.menu-btn')
-const mobileMenus = document.querySelectorAll('.mobile-menu')
-
-menuButtons.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        mobileMenus[index].classList.toggle('hidden')
-    })
-})
+    menuButtons.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            mobileMenus[index].classList.toggle('hidden');
+        });
+    });
