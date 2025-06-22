c1860648f23d1fc60cf111af2ec3336a5d54f1c5
Knight <101966299+KN1-GHT@users.noreply.github.com>
2025-06-22 13:37:25 +0545

    added multiple modals and made several pages dynamic.

--- theme/admin.py
+++ theme/admin.py
@@ -16,3 +16,21 @@ class NewsItemAdmin(admin.ModelAdmin):
     search_fields = ('title',)
     list_filter = ('published_date',)
     ordering = ('-published_date',)
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+    extra = 1
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [PhotoInline]
+
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Photo)
+
+@admin.register(HomepageNews)
+class HomepageNewsAdmin(admin.ModelAdmin):
+    list_display = ('message', 'date')
+    ordering = ('-date',)
+    search_fields = ('message',)
